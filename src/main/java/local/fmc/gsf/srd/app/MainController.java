bf06b4501815efafa0efc15dbec178c83dd56bb7
Fernando de Alcântara Cadalso <fernando.cadalsoii@gmail.com>
2021-08-23 23:41:11 -0300

    Deploy atual

--- src/main/java/local/fmc/gsf/srd/app/MainController.java
+++ src/main/java/local/fmc/gsf/srd/app/MainController.java
@@ -1,19 +1,8 @@
 package local.fmc.gsf.srd.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import local.fmc.gsf.srd.dominio.Dispensa;
-import local.fmc.gsf.srd.dominio.DispensaBuilder;
-import local.fmc.gsf.srd.dominio.ItemDeConsumo;
-import local.fmc.gsf.srd.dominio.ItemBuilder;
-import local.fmc.gsf.srd.infra.DispensaRepo;
-import local.fmc.gsf.srd.infra.ItemDeConsumoRepo;
 
 @Controller
 @RequestMapping(path = "/")
